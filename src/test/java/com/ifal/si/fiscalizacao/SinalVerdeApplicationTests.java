213a3191d7c021b14f71ccc781ec6cec004a2b34
LuizRoberto18 <luiz.roberto20@yahoo.com>
2025-12-09 11:33:16 -0300

    incializando Spring boot, configuração back, front e database

---
+++ src/test/java/com/ifal/si/fiscalizacao/SinalVerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifal.si.fiscalizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinalVerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
